3a49766918bab0d13f97d7466eef92d08cfd31cb
emriys <bodeokunadeab@gmail.com>
2025-01-27 19:24:31 +0100

    Commit before first deployment: update 2

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def refresh_session():
 # ---------------- RUN APPLICATION ---------------- #
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=False, threaded=True)
+    app.run()
